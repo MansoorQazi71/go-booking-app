8fd7a396e70be346d7c4c90cde4ebcfef010ff4f
MansoorAhmadQazi <mansoorqazi71@gmail.com>
2025-06-24 13:31:06 +0500

    if condition and contional for loop

--- main.go
+++ main.go
@@ -15,26 +15,34 @@ func main() {
 	var bookings []string
 
 	fmt.Println("Welcome to ", conferenceName, "booking application")
-	for {
+	for remainingTickets > 0 && len(bookings) < 50 {
 		fmt.Println("please enter your first name:")
 		fmt.Scan(&firstName)
 		fmt.Println("please enter your last name:")
 		fmt.Scan(&lastName)
-		fmt.Println("please enter number of tickets you want to book:")
-		fmt.Scan(&userTickets)
 		fmt.Println("Please enter your email address:")
 		fmt.Scan(&email)
-		bookings = append(bookings, firstName+" "+lastName)
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+		fmt.Println("please enter number of tickets you want to book:")
+		fmt.Scan(&userTickets)
+		if userTickets <= remainingTickets {
+			bookings = append(bookings, firstName+" "+lastName)
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("booking details: %v\n", firstNames)
+			if remainingTickets == 0 {
+				fmt.Println("All tickets are sold out for", conferenceName)
+				break
+			}
+			fmt.Println("Thank you", firstName, lastName, "for booking", userTickets, "tickets for", conferenceName)
+			fmt.Printf("You will receive a confirmation email on %v at the end of the booking process.\n", email)
+			remainingTickets = remainingTickets - userTickets
+			fmt.Printf("Remaining tickets for %v are %v\n", conferenceName, remainingTickets)
+		} else {
+			fmt.Printf("Sorry, we only have %v tickets remaining for %v\n", remainingTickets, conferenceName)
 		}
-		fmt.Printf("booking details: %v\n", firstNames)
-		fmt.Println("Thank you", firstName, lastName, "for booking", userTickets, "tickets for", conferenceName)
-		fmt.Printf("You will receive a confirmation email on %v at the end of the booking process.\n", email)
-		remainingTickets = remainingTickets - userTickets
-		fmt.Printf("Remaining tickets for %v are %v\n", conferenceName, remainingTickets)
 	}
 
 }
